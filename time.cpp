c48404e6f77164dba28c71f062bc55c645e8950d
claranobre <aclaranobre@gmail.com>
2016-05-04 16:39:26 -0300

    Adicionando função do tempo - dada pelo professor

---
+++ time.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <chrono>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    auto t1 = std::chrono::high_resolution_clock::now();
+
+    int i;
+    cout << "Digite um valor ineiro\n";
+    cin >> i;
+
+    auto t2 =  std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> tempo = t2 - t1;
+    cout << "Tempo total gasto na digitação: " << tempo.count() << "\n";
+    return 0;
+}
+
